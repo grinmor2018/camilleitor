ec35d68692f7d726358b814f8f35336ab53b0b89
David Marquez <grinmor2016@gmail.com>
2020-09-15 12:12:29 +0200

    mes habitacions

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -22,7 +22,7 @@ export class TasksComponent implements OnInit {
   transportList: string[] = ['Camilla', 'A peu', 'Llit', 'Cadira'];
   estatList: string[][]= [['Demanat','red'], ['Portat','cyan'], ['Tornat','green']];
   destinationList: string[] = ['RESO 1', 'RESO 2', 'TAC Tauli', 'TAC UDIAT', 'RX CENTRAL', 'RX UGCES'];
-  roomList: any = rooms;
+  roomList: any = rooms.values;
   hiddenState:boolean = true;
   hiddenButton:boolean =  !(this.hiddenState);
 
