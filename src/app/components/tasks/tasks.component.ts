1b1bfb6159b532f5df2e218e532f109e4a59559b
David Marquez <grinmor2016@gmail.com>
2020-10-08 12:23:24 +0200

    set focus

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -33,6 +33,7 @@ export class TasksComponent implements OnInit {
   addTask(form?: NgForm) {
     console.log(form.value);
     this.hiddenState=false;
+    this.focusMethod();
     if (form.value._id) {
       this.taskService.putTask(form.value).subscribe((res) => {
         this.resetForm(form);
@@ -64,7 +65,6 @@ export class TasksComponent implements OnInit {
     this.hiddenState=false;
     this.hiddenButton=true;
     this.hiddenButtons=false;
-    this.focusMethod();
   }
 
   deleteTask(_id: string, form: NgForm) {
